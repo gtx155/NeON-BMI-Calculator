1d44646b879ee8ffaef9f4d32c8b03f3bd1bbe72
GTX155 <kirchoabv@gmail.com>
2023-07-28 14:45:03 +0300

    naming now in camelcase, wallpaper mode fixes

--- script.js
+++ script.js
@@ -1,70 +1,66 @@
 document.getElementById("BMIForm").addEventListener("submit", function(event){
   event.preventDefault();
 
-  let Height = document.getElementById("heightInput").value;
-  let Weight = document.getElementById("weightInput").value;
+  let height = document.getElementById("heightInput").value;
+  let weight = document.getElementById("weightInput").value;
       
-  let Height2 = (Height / 100) ** 2;
-  let BMIResult = Weight / Height2;
+  let heightCalc = (height / 100) ** 2;
+  let bmiResult = weight / heightCalc;
   
-  let BMIRoundDown = Math.floor(BMIResult * 100) / 100;
-  
-
+  let bmiRoundDown = Math.floor(bmiResult * 100) / 100;
 
   //bmi var to html
-  let htmlBMIVar = "BMI = " + BMIRoundDown;
-  let BMITextElement = document.getElementById("BMIText");
-  BMITextElement.innerHTML = htmlBMIVar;
+  let htmlBMIVar = "BMI = " + bmiRoundDown;
+  let bmiTextElement = document.getElementById("BMIText");
+  bmiTextElement.innerHTML = htmlBMIVar;
 
-  if (BMIRoundDown < 18.5) {
-    var FloatIcon = document.getElementById("float");
-    FloatIcon.classList.remove("green", "red", "yellow");
-    FloatIcon.classList.add("orange");
-    FloatIcon.textContent = "Underweight";
+  if (bmiRoundDown < 18.5) {
+    var floatIcon = document.getElementById("float");
+    floatIcon.classList.remove("green", "red", "yellow");
+    floatIcon.classList.add("orange");
+    floatIcon.textContent = "Underweight";
 
-    var Platform = document.getElementById("platform");
-    Platform.classList.add("platform");
-  } else if (BMIRoundDown >= 18.5 && BMIRoundDown <= 25) {
-    var FloatIcon = document.getElementById("float");
-    FloatIcon.classList.remove("orange", "red", "yellow");
-    FloatIcon.classList.add("green");
-    FloatIcon.textContent = "Healthy weight";
+    var platform = document.getElementById("platform");
+    platform.classList.add("platform");
+  } else if (bmiRoundDown >= 18.5 && bmiRoundDown <= 25) {
+    var floatIcon = document.getElementById("float");
+    floatIcon.classList.remove("orange", "red", "yellow");
+    floatIcon.classList.add("green");
+    floatIcon.textContent = "Healthy weight";
 
-    var Platform = document.getElementById("platform");
-    Platform.classList.add("platform");
+    var platform = document.getElementById("platform");
+    platform.classList.add("platform");
 
-    } else if (BMIRoundDown >= 25 && BMIRoundDown <= 30) {
-    var FloatIcon = document.getElementById("float");
-    FloatIcon.classList.remove("orange", "red", "green");
-    FloatIcon.classList.add("yellow");
-    FloatIcon.textContent = "Overweight";
+    } else if (bmiRoundDown >= 25 && bmiRoundDown <= 30) {
+    var floatIcon = document.getElementById("float");
+    floatIcon.classList.remove("orange", "red", "green");
+    floatIcon.classList.add("yellow");
+    floatIcon.textContent = "Overweight";
 
-    var Platform = document.getElementById("platform");
-    Platform.classList.add("platform");
+    var platform = document.getElementById("platform");
+    platform.classList.add("platform");
 
-    } else if (BMIRoundDown > 30) {
-      var FloatIcon = document.getElementById("float");
-      FloatIcon.classList.remove("orange", "green", "yellow");
-      FloatIcon.classList.add("red");
-      FloatIcon.textContent = "Obese";
+    } else if (bmiRoundDown > 30) {
+      var floatIcon = document.getElementById("float");
+      floatIcon.classList.remove("orange", "green", "yellow");
+      floatIcon.classList.add("red");
+      floatIcon.textContent = "Obese";
 
-      var Platform = document.getElementById("platform");
-      Platform.classList.add("platform");
+      var platform = document.getElementById("platform");
+      platform.classList.add("platform");
     }
   }
 );
 
-
-
 //get all the elements that can change theme
-const ThemedElmSelector = document.getElementsByName("themed");
+const themedElmSelector = document.getElementsByName("themed");
 
 const argon = document.querySelector('.background li:nth-child(1)');
 argon.addEventListener('click', function() {
   argon.classList.add("argon");
   argon.style.backgroundColor = '#A850F0';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("neon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "xenon", "krypton", "helium", "cesium");
   element.classList.add("argon");
 });
@@ -76,7 +72,7 @@ neon.addEventListener('click', function() {
   neon.classList.add("neon");
   neon.style.backgroundColor = '#FC321A';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "xenon", "krypton", "helium", "cesium");
   element.classList.add("neon");
 });
@@ -87,7 +83,7 @@ helium.addEventListener('click', function() {
   helium.classList.add("helium");
   helium.style.backgroundColor = '#D5443E';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "xenon", "krypton", "cesium");
   element.classList.add("helium");
 });
@@ -98,7 +94,7 @@ krypton.addEventListener('click', function() {
   krypton.classList.add("krypton");
   krypton.style.backgroundColor = '#C2B3C8';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "xenon", "helium", "cesium");
   element.classList.add("krypton");
 });
@@ -109,7 +105,7 @@ xenon.addEventListener('click', function() {
   xenon.classList.add("xenon");
   xenon.style.backgroundColor = '#4467B2';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "krypton", "helium", "cesium");
   element.classList.add("xenon");
 });
@@ -120,7 +116,7 @@ hydrogen.addEventListener('click', function() {
   hydrogen.classList.add("hydrogen");
   hydrogen.style.backgroundColor = '#E5B1F5';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "mercury-vapor", "xenon", "krypton", "helium", "cesium");
   element.classList.add("hydrogen");
 });
@@ -131,7 +127,7 @@ mercuryVapor.addEventListener('click', function() {
   mercuryVapor.classList.add("mercury-vapor");
   mercuryVapor.style.backgroundColor = '#52E896';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "hydrogen", "xenon", "krypton", "helium", "cesium");
   element.classList.add("mercury-vapor");
 });
@@ -142,7 +138,7 @@ nitrogen.addEventListener('click', function() {
   nitrogen.classList.add("nitrogen");
   nitrogen.style.backgroundColor = '#B83579';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "mercury-vapor", "hydrogen", "xenon", "krypton", "helium", "cesium");
   element.classList.add("nitrogen");
 });
@@ -153,7 +149,7 @@ cesium.addEventListener('click', function() {
   cesium.classList.add("cesium");
   cesium.style.backgroundColor = '#AEB4FF';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "radon", "nitrogen", "mercury-vapor", "hydrogen", "xenon", "krypton", "helium");
   element.classList.add("cesium");
 });
@@ -164,7 +160,7 @@ radon.addEventListener('click', function() {
   radon.classList.add("radon");
   radon.style.backgroundColor = '#E81828';
 
-[...ThemedElmSelector].forEach(element => {
+[...themedElmSelector].forEach(element => {
   element.classList.remove("argon", "neon", "mercury-vapor", "hydrogen", "xenon", "krypton", "helium", "cesium", "nitrogen");
   element.classList.add("radon");
 });
@@ -201,16 +197,19 @@ function toggleAMOLED() {
   });
 }
 
+//Wallpaper Mode
 const buttonSubmit = document.querySelector('.submit-button');
+const float = document.querySelector('#float');
+const platform = document.querySelector('#platform');
 
 const toBeHiddens = [...h1s, ...inputs]; 
 
-//Wallpaper Mode
 function toggleWallpaper() {
   buttonSubmit.classList.toggle('hide');
+  float.classList.toggle('hide');
+  platform.classList.toggle('hide');
 
   toBeHiddens.forEach(toBeHidden => {
     toBeHidden.classList.toggle('hide');
   });
 }
-
